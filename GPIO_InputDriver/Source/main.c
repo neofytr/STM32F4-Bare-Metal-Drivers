ed149af4170e6db10bc46f0251815432579e1a48
RajDaBest <imagination12344321.com@gmail.com>
2024-11-01 19:25:37 +0530

    GPIO Input Driver

--- GPIO_InputDriver/Source/main.c
+++ GPIO_InputDriver/Source/main.c
@@ -14,10 +14,33 @@
 
 // Bit two of RCC-AHB1ENR (if set) enables the clock access to GPIOC
 
-#define GPIOC_ENR (1UL << 2)
+#define GPIOA_EN (1UL << 0)
+#define PIN5 (1UL << 5)
 
+#define GPIOC_EN (1UL << 2)
+#define PIN13 (1UL << 13)
+
+#define BTN_PIN PIN13
+#define LED_PIN PIN5
 
 int main(void)
 {
-    RCC->AHB1ENR |= GPIOC_ENR;
+    // enable clock access to GPIOA and GPIOC
+    RCC->AHB1ENR |= GPIOC_EN;
+    RCC->AHB1ENR |= GPIOA_EN;
+
+    // set PA5 as output pin (can be done in MODE register of GPIOA)
+    // for output: 01
+    // for pin 5: bit 11 bit 10
+    GPIOA->MODER |= (1UL << 10);    // sets the 10th bit to 1
+    GPIOA->MODER &= (~(1UL << 11)); // sets the 11th bit to 0
+
+    // set PC13 as input pin
+    // for input: 00
+    // for pin 13: bit 27 bit 26
+    GPIOC->MODER &= (~(1UL << 26));
+    GPIOC->MODER &= (~(1UL << 27));
+    // we didn't really need to do this since the reset state of GPIOC MODE register is all zeroes
+
+    
 }
