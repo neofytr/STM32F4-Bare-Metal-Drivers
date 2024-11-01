6bb7716a57b9897b0b3c1ddf0075904b5486c71c
RajDaBest <imagination12344321.com@gmail.com>
2024-11-01 06:56:01 +0530

    added core includes

--- Source/main.c
+++ Source/main.c
@@ -5,43 +5,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include "../Includes/STM32F401.h"
-
-#define PERIPH_BASE (0x40000000UL) // base for all the peripherals
-
-#define AHB1_PERIPH_OFFSET (0x20000UL)
-#define AHB1_BASE (PERIPH_BASE + AHB1_PERIPH_OFFSET) // base for all the peripherals on the AHB1 bus
-
-#define GPIOA_AHB1_OFFSET (0x0UL)
-#define GPIOA_BASE (AHB1_BASE + GPIOA_AHB1_OFFSET) // base for the GPIOA port configuration
-
-#define RCC_AHB1_OFFSET (0x3800UL) 
-#define RCC_BASE (AHB1_BASE + RCC_AHB1_OFFSET) // base of the RCC peripheral configuration
-
-// setting the bits [11:10] of the GPIOA Mode register to 01 puts the 5th pin of GPIOA into output mode
-
-#define PIN5_OUT ((01UL) << 10)
+#include "../Includes/core/core_cm4.h"
 
 #define GPIOA_EN (1UL << 0) // first bit in the AHB1 enable register is for enabling GPIOA
 #define PIN5 (1UL << 5)     // 5th bit in the GPIOA output data register is for sending data to PIN5
+#define PIN5_OUT (01UL << 10)
 
 #define __IO volatile
 
-typedef struct
-{
-    __IO uint32_t MODER; // mode register
-    __IO uint32_t DUMMY[4];
-    __IO uint32_t ODR;
-} GPIO_;
-
-typedef struct
-{
-    __IO uint32_t DUMMY[12];
-    __IO uint32_t AHB1ENR;
-} RCC_;
-
-#define RCC ((RCC_ *)RCC_BASE)
-#define GPIOA ((GPIO_ *)GPIOA_BASE)
 
 int main(void)
 {
